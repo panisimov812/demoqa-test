d2fd6ba49065ea139a0b5bd504b94208978028a1
panisimov812 <panisimov@gid.ru>
2022-11-08 00:11:03 +0300

    added test case fillFormTest

--- src/test/java/demoqa/Junit5Tests.java
+++ src/test/java/demoqa/Junit5Tests.java
@@ -1,12 +1,38 @@
 package demoqa;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Junit5Tests {
 
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "http://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
     @Test
     void fillFormTest() {
-        open("http://demoqa.com/text-box");
+        // 1:45:10
+        String name = "Test Name";
+        String email = "test@email.de";
+        String address = "test Address 3";
+        open("/text-box");
+
+        $("[id=userName]").setValue(name);
+        $("[id=userEmail]").setValue(email);
+        $("[id=currentAddress]").setValue(address);
+        $("[id=permanentAddress]").setValue("test Address 33");
+        $("[id=submit]").click();
+
+        $("[id=output] [id=name]").shouldHave(text(name));
+       // $("[id=output]").$("[id=name]").shouldHave(text(name));
     }
 }
